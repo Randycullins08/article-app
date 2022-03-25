a214acbe399da289ca9b4f8d63750e828d4276e9
Randy Cullins <randycullins2018@gmail.com>
2022-03-25 06:15:51 -0600

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+export default function Navbar() {
+  return (
+    <div className="navbar-container">
+      <div className="top-logo-wrapper">
+        <img src="images/logo.png" alt="Site Logo" />
+      </div>
+
+      <div className="buttons-wrapper">
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+      </div>
+
+      <div className="bottom-logo-wrapper">
+        <img src="images/logo.png" alt="Site Logo" />
+      </div>
+    </div>
+  );
+}
