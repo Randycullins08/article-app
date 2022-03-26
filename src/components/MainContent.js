f4896a0b0db1b8c0d93d51485546ab43e6793f5d
Randy Cullins <randycullins2018@gmail.com>
2022-03-26 07:20:18 -0600

    main content start

---
+++ src/components/MainContent.js
@@ -0,0 +1,113 @@
+export default function MainContent() {
+  return (
+    <div className="main-content-container">
+      <div className="top-page-image-wrapper">
+        <img
+          src="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
+          alt="MAC IS SUPERIOR"
+        />
+      </div>
+
+      <div className="header-wrapper">
+        <h2>Tech Article</h2>
+      </div>
+
+      <div className="sub-header-wrapper">
+        <h4>- Article about tech</h4>
+        <hr />
+      </div>
+
+      <div className="top-main-content-wrapper">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quod et
+          odit impedit dolores totam ullam ab quaerat unde cum, numquam nihil
+          non, ducimus quos at aspernatur fuga vel cupiditate? Omnis laboriosam
+          sed perferendis quaerat cupiditate, eos veniam quasi nesciunt sit iste
+          unde ipsam laudantium natus eligendi adipisci illum nulla debitis.
+          Numquam culpa cupiditate tempore quis natus, modi vero vel. Itaque, et
+          saepe accusantium reiciendis, ut temporibus repellendus cupiditate
+          voluptatibus reprehenderit ullam dolor velit! Id et ab optio aliquid
+          asperiores cum dolorum reiciendis modi a. Omnis fuga beatae qui
+          delectus! Inventore optio labore suscipit minus aspernatur tenetur
+          magni facere laudantium non illo voluptates possimus similique ab vel
+          incidunt porro, aut ut, natus maxime, assumenda exercitationem est
+          nisi laboriosam! Explicabo, sunt! Nihil esse laboriosam aperiam quae
+          enim dolores voluptas, molestias repellat quis nemo quos unde ad eius
+          facere provident illo quo dolorem accusamus minus harum illum soluta
+          rerum dolorum praesentium. Optio? Consectetur tempora dolorem
+          cupiditate placeat officia beatae cum distinctio porro quidem fugiat,
+          corporis eum vitae quo! Non autem fugiat similique quaerat laborum
+          laboriosam, numquam reprehenderit repellat, quisquam dignissimos,
+          perferendis quidem. Rerum, maxime? Eos consectetur excepturi culpa
+          provident fugiat! Aspernatur, in nostrum commodi veritatis fuga unde
+          cupiditate sequi incidunt quaerat ipsa.
+        </p>
+        <hr />
+      </div>
+
+      <div className="middle-content-wrapper">
+        <div className="content-wrapper">
+          <div className="content-header">
+            <h3>Header</h3>
+            <hr width="20%" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quod
+              et odit impedit dolores totam ullam ab quaerat unde cum, numquam
+              nihil non, ducimus quos at aspernatur fuga vel cupiditate? Omnis
+              laboriosam sed perferendis quaerat cupiditate, eos veniam quasi
+              nesciunt sit iste unde ipsam laudantium natus eligendi adipisci
+              illum nulla debitis. Numquam culpa cupiditate tempore quis natus,
+              modi vero vel.
+            </p>
+            <hr />
+          </div>
+        </div>
+        <img
+          className="top-right-image"
+          src="https://images.unsplash.com/photo-1504639725590-34d0984388bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+          alt="tech with glasses"
+        />
+        <img
+          className="bottom-left-image"
+          src="https://images.unsplash.com/photo-1468436139062-f60a71c5c892?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          alt="mac"
+        />
+        <div className="content-wrapper">
+          <div className="content-header">
+            <h3>Header</h3>
+            <hr width="20%" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quod
+              et odit impedit dolores totam ullam ab quaerat unde cum, numquam
+              nihil non, ducimus quos at aspernatur fuga vel cupiditate? Omnis
+              laboriosam sed perferendis quaerat cupiditate, eos veniam quasi
+              nesciunt sit iste unde ipsam laudantium natus eligendi adipisci
+              illum nulla debitis. Numquam culpa cupiditate tempore quis natus,
+              modi vero vel.
+            </p>
+            <hr />
+          </div>
+        </div>
+      </div>
+
+      <div className="bottom-content-wrapper">
+        <h3>MAIN CONTENT</h3>
+      </div>
+
+      <div className="button-wrapper">
+        <button>Button</button>
+      </div>
+
+      <div className="social-media-icon-wrapper">
+        <i class="fab fa-facebook-f"></i>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-linkedin"></i>
+        <i class="fab fa-github"></i>
+      </div>
+
+      <div className="footer">
+        <p>Copyright Randy Cullins</p>
+      </div>
+    </div>
+  );
+}
